4ed5847a978c47a884c43bed82b365626ae53bc2
ThataBispo <thauane05.bs@gmail.com>
2023-10-19 16:43:01 -0300

    conexoes

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, request, response } from "express";
 import db from "../database/connection";
 import convertHourToMinutes from "../utils/convertHourToMinutes";
 
@@ -12,17 +12,32 @@ export default class ClassesController {
   async index(request: Request, response: Response) {
     const filters = request.query;
 
+    const subject = filters.subject as string;
+    const week_day = filters.week_day as string;
+    const time = filters.time as string;
+
     if (!filters.week_day || !filters.subject || !filters.time) {
       return response.status(400).json({
         error: "Missing filters to search classes",
       });
     }
 
-    const timeInMinutes = convertHourToMinutes(filters.time as string);
+    const timeInMinutes = convertHourToMinutes(time);
 
-    console.log(timeInMinutes);
+    const classes = await db("classes")
+      .whereExists(function () {
+        this.select("class_schedule.*")
+          .from("class_schedule")
+          .whereRaw("`class_schedule`.`class_id`  = `classes`.`id`")
+          .whereRaw("`class_schedule`.`week_day` = ??", [Number(week_day)])
+          .whereRaw("`class_schedule`.`from` <= ??", [timeInMinutes])
+          .whereRaw("`class_schedule`.`to` > ??", [timeInMinutes]);
+      })
+      .where("classes.subject", "=", subject)
+      .join("users", "classes.user_id", "=", "users.id")
+      .select(["classes.*", "users.*"]);
 
-    return response.send();
+    return response.json(classes);
   }
 
   async create(request: Request, response: Response) {
@@ -64,6 +79,8 @@ export default class ClassesController {
 
       return response.status(201).send();
     } catch (err) {
+      console.log(err);
+
       await trx.rollback();
 
       return response.status(400).json({
